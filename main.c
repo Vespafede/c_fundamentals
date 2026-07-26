fa20b390f30cad2dca3205ffb0329283f51f574f
Federico Grosso <federico.grosso2008@gmail.com>
2026-07-27 00:50:49 +0200

    done

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, C!\n");    
+    int a = 0, b = 0;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d\n", a + b);
 
     return 0;
 }
