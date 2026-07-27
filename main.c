416e11c48c33608b2933e47b1fabed8f3c1fb61b
Federico Grosso <federico.grosso2008@gmail.com>
2026-07-27 07:59:27 +0200

    done

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
-typedef struct {
-    int x, y;
-} Point;
+#include <stdlib.h>
 
 int main(void) {
-    int x1, y1, x2, y2;
-    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    int n;
+    scanf("%d", &n);
+    int* nums = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) scanf("%d", &nums[i]);
     
-    Point p1 = {x1, y1};
-    Point p2 = {x2, y2};
+    int somma = 0;
+    for (int i = 0; i < n; i++) {
+        somma += nums[i];
+    }
 
-    int distance = (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
-
-    printf("%d\n", distance);
+    printf("%d\n", somma);
 
     return 0;
 }
