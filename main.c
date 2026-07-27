cb671a1f0ee7c1175526b8a5cfd07f283a83d741
Federico Grosso <federico.grosso2008@gmail.com>
2026-07-27 07:41:38 +0200

    done

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-void swap(int* a, int* b) {
-    int swap_var = *a;
-    *a = *b;
-    *b = swap_var;
-}
+typedef struct {
+    int x, y;
+} Point;
 
 int main(void) {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    swap(&a, &b);
-    printf("%d %d\n", a, b);
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    
+    Point p1 = {x1, y1};
+    Point p2 = {x2, y2};
+
+    int distance = pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2);
+
+    printf("%d\n", distance);
+
     return 0;
 }
